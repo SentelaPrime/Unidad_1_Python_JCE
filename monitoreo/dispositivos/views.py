3ef645801f26ebb8e80cf148eb7a141a0313306d
SentelaPrime <joaquincorteserices@gmail.com>
2025-08-22 11:47:06 -0400

    Tarea 1

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def inicio(request):
+    contexto = {"nombre": "Profe Javier"}
+    productos = [
+        {"nombre": "Sensor 1", "valor": 100},
+        {"nombre": "Sensor 2", "valor": 200},
+        {"nombre": "Sensor 3", "valor": 300}
+    ]
+    
+    return render(request, "dispositivos/inicio.html", {"contexto": contexto, "productos": productos})
+
+
+def panel_dispositivos(request):
+    dispositivos = [
+        {"nombre": "Sensor Temperatura", "consumo": 50},
+        {"nombre": "Medidor Solar", "consumo": 120},
+        {"nombre": "Sensor Movimiento", "consumo": 30},
+        {"nombre": "Calefactor", "consumo": 200}
+    ]
+
+    consumo_maximo = 100
+
+    return render(request, "dispositivos/panel.html", 
+                  {"dispositivos": dispositivos, 
+                   "consumo_maximo": consumo_maximo})
